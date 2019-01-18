c2297c0762d65204544d5b23bc3a01c5c18506ef
PanJtao <PanJtao@outlook.com>
2019-01-18 18:05:12 +0800

    Initial commit

---
+++ src/com/pjt/FirstGit.java
@@ -0,0 +1,11 @@
+package com.pjt;
+
+/**
+ * @author PanJtao
+ * @date 2019/1/18 18:00
+ */
+public class FirstGit {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
